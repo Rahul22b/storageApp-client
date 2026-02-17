4b00cd8b02fea8c94f7699125b45164762dfde66
rahul22b <rahul2212005@gmail.com>
2026-02-18 01:35:36 +0530

    checking deployment

--- src/components/GuestPage.jsx
+++ src/components/GuestPage.jsx
@@ -76,7 +76,7 @@ export default function GuestLandingPage() {
             <div className="flex items-center space-x-2">
               <Cloud className="w-8 h-8 text-blue-500" />
               <span className="text-xl font-bold bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">
-                cloud storage
+                cloud App
               </span>
             </div>
 
