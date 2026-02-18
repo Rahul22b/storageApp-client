a9772cf03f4319d2f2e1ef7c82a22c815b789632
rahul22b <rahul2212005@gmail.com>
2026-02-18 09:17:07 +0530

    hhf

--- src/components/GuestPage.jsx
+++ src/components/GuestPage.jsx
@@ -303,7 +303,7 @@ export default function GuestLandingPage() {
             </div>
           </div>
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2024 CloudStore. All rights reserved.</p>
+            <p>&copy; 2027 CloudStore. All rights reserved.</p>
           </div>
         </div>
       </footer>
