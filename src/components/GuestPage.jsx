227f594040d05aad358bd9757ae51a9c6a526132
rahul22b <rahul2212005@gmail.com>
2026-02-20 17:24:39 +0530

    dhd

--- src/components/GuestPage.jsx
+++ src/components/GuestPage.jsx
@@ -303,7 +303,7 @@ export default function GuestLandingPage() {
             </div>
           </div>
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2027 Rimsha Abiba. All rights reserved.</p>
+            <p>&copy; 2027 Sana Adiba . All rights reserved.</p>
           </div>
         </div>
       </footer>
